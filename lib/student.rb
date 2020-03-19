de97df7af104767a37b451fa4cab8c2dac6cbac0
Ryna Truong <rynatruong@outlook.com>
2020-03-19 22:24:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+  
+  attr_accessor :first_name, :last_name
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(string_of_knowledge)
+    @knowledge << string_of_knowledge
+  end
+  
+  def knowledge
+    @knowledge
+  end
+
 
 end
